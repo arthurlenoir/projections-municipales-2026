fceb685146fc475cef6036943b2e08957f0ac93c
Arthur Lenoir <me@arthurlenoir.fr>
2026-03-16 19:46:24 +0100

    feat: enable deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.CI ? '/projections-munucipales/' : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
