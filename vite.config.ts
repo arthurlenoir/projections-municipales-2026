1f8af05977677b32e96b94668e853d5f2c61208d
Arthur Lenoir <me@arthurlenoir.fr>
2026-03-16 20:03:45 +0100

    fix: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.CI ? '/projections-munucipales/' : '/',
+  base: process.env.CI ? '/projections-municipales-2026/' : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
